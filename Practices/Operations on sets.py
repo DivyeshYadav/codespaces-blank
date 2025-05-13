0a7d59cd758c2c1fd069315bd093c96767856cb8
DivyeshYadav <prabhakaryadav621@gmail.com>
2025-05-13 15:49:22 +0000

    Operation on  sets

---
+++ Practices/Operations on sets.py
@@ -0,0 +1,14 @@
+s1 = {1,2,3,4,5,6,7,7,7,7,8,9,10}
+s2 = {3,5,7,39,7,4,4,7,8,9,9,10,2}
+
+print("Set 1: ",s1)
+print("Set 2: ",s2)
+
+print("Intersection: ")
+print(s1.intersection(s2))
+print("\nUnion: ")
+print(s1.union(s2))
+print("\nDifference: ",)
+print(s1.difference(s2))
+print("\nSymmetric Difference: ")
+print(s1.symmetric_difference(s2))
