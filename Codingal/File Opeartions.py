38386f5762f0f40a81cb9ca4f55cdda126c450aa
DivyeshYadav <prabhakaryadav621@gmail.com>
2025-09-18 19:20:56 +0545

    Remove lines

--- Codingal/File Opeartions.py
+++ Codingal/File Opeartions.py
@@ -19,3 +19,18 @@ f.close()
 
 f = open("Tiger.txt", "r")
 print(f.read(20))
+f.close()
+
+
+f1  = open("Tiger.txt", "r")
+f2 = open("UpdatedTiger.txt", "w")
+for i in f1.readlines():
+    if not(i.startswith("The")):
+        f2.write(i)
+
+f1.close()
+f2.close()
+
+f = open("UpdatedTiger.txt", "r")
+print(f.readlines())
+f.close()
