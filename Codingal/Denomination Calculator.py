a50fabafe7969800f294906f26c3614427ef4837
DivyeshYadav <prabhakaryadav621@gmail.com>
2025-10-16 19:22:34 +0545

    Denomination Calculator

---
+++ Codingal/Denomination Calculator.py
@@ -0,0 +1,113 @@
+from tkinter import *
+from tkinter import messagebox
+from PIL import Image, ImageTk
+
+window = Tk()
+window.title("Denomination Calculator")
+window.configure(bg="light blue")
+window.geometry("650x400")
+
+upload = Image.open("Codingal\Giraffe.jpg")
+Image = ImageTk.PhotoImage(upload)
+label = Label(window, image=Image, bg="light blue")
+label.place(x=100, y=20)
+
+label1 = Label(window, text="Hi welcome the Denomination Calculator", bg="light blue")
+label1.place(relx=0.5,y=340)
+
+def msg():
+    Msgbox = messagebox.showinfo('Alert',"Do you want to calculate the denomination count?")
+    topwin()
+
+button = Button(window, text="Let's get started", command=msg,bg ="brown",fg="white")
+button.place(x=260,y=360)
+
+def topwin():
+    top = Toplevel(window)
+    top.title("Denomination Calculator")
+    top.configure(bg = "light grey")
+    top.geometry("600x350+50+50")
+
+    label = Label(top, text="Enter the total amount", bg = "light grey")
+    entry = Entry(top)
+    lbl = Label(top, text="Here are the number of notes for each denomination", bg = "light grey")
+
+    l1 = Label(top, text="1000", bg = "light grey")
+    l2 = Label(top, text="500", bg = "light grey")
+    l3 = Label(top, text="100", bg = "light grey")
+    l4 = Label(top, text="50", bg = "light grey")
+    l5 = Label(top, text="20", bg = "light grey")
+    l6 = Label(top, text="10", bg = "light grey")
+    l7 = Label(top, text="5", bg = "light grey")
+
+    t1 = Entry(top)
+    t2 = Entry(top)
+    t3 = Entry(top)
+    t4 = Entry(top)
+    t5 = Entry(top)
+    t6 = Entry(top)
+    t7 = Entry(top)
+
+
+    def calculator():
+        try:
+            global amount
+            amount = int(entry.get())
+            note1000 = amount // 1000
+            amount %= 1000
+            note500 = amount // 500
+            amount %= 500
+            note100 = amount // 100
+            amount %= 100
+            note50 = amount //  50
+            amount %= 50
+            note20 = amount // 20
+            amount %= 20
+            note10 = amount // 10
+            amount %= 10
+            note5 = amount // 5
+
+            t1.delete(0, END)
+            t2.delete(0, END)
+            t3.delete(0, END)
+            t4.delete(0, END)
+            t5.delete(0, END)
+            t6.delete(0, END)
+            t7.delete(0, END)
+
+            t1.insert(END, str(note1000))
+            t2.insert(END, str(note500))
+            t3.insert(END, str(note100))
+            t4.insert(END, str(note50))
+            t5.insert(END, str(note20))
+            t6.insert(END, str(note10))
+            t7.insert(END, str(note5))
+        except ValueError:
+            messagebox.showerror("Error", "Please enter a valid number.")
+
+    btn = Button(top, text="Calacualate", command=calculator, bg="brown", fg="white")
+    label.place(x=230, y=50)
+    entry.place(x=200, y=80)
+    btn.place(x=240, y=120)
+    lbl.place(x=140, y=170)
+
+    l1.place(x=180, y=200)
+    l2.place(x=180, y=230)
+    l3.place(x=180, y=260)
+    l4.place(x=180, y=290)
+    l5.place(x=180, y=310)
+    l6.place(x=180, y=340)
+    l7.place(x=180, y=370)
+
+    t1.place(x=270, y=200)
+    t2.place(x=270, y=230)
+    t3.place(x=270, y=260)
+    t4.place(x=270, y=290)
+    t5.place(x=270, y=310)
+    t6.place(x=270, y=340)
+    t7.place(x=270, y=370)
+
+    top.mainloop()
+        
+window.mainloop()        
+
